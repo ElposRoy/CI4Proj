beb06b9c54c81308bd4cec7c0c92e25643ca6b0a
Moixie <royelpos@gmail.com>
2023-12-06 14:11:01 +0800

    ...

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,15 @@ class Home extends BaseController
 {
     public function index(): string
     {
+
+        $db = \Config\Database::connect();
+        $builder = $db-> table('authors');
+
+        $query = $builder -> get();
+
+        $result = $query -> getResult();
+        return json_encode($result);
+
         return view('welcome_message');
     }
 }
